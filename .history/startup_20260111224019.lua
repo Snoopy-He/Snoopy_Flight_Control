d327e55e4b5bb91218a42da102ebc4abd89e95f2
Snoopy_He <2935881650@qq.com>
2026-01-11 22:44:15 +0100

    新建文件夹，顺便完成了电机标号

--- .history/startup_20260111224019.lua
+++ .history/startup_20260111224019.lua
@@ -4,7 +4,6 @@ local Motor1 = Motor[1]   --负转速
 local Motor2 = Motor[2]   --正转速
 local Motor3 = Motor[4]   --正转速
 local Motor4 = Motor[3]   --负转速
-
 while true do
     Motor1.setTargetSpeed(-0)
     Motor2.setTargetSpeed(0)
