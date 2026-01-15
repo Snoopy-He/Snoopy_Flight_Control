fe9625d3fbe66cfda4f33b10c15d6fa0ad4251ab
Snoopy_He <2935881650@qq.com>
2026-01-15 01:54:15 +0100

    初步实现定点悬停，有待完善算法和调参

--- startup.lua
+++ startup.lua
@@ -1,9 +1,11 @@
 print("hello flight control!")
 local Motor={peripheral.find("Create_RotationSpeedController")}
-local Motor1 = Motor[1]   --负转速        3      1
-local Motor2 = Motor[2]   --正转速
-local Motor3 = Motor[4]   --正转速        2      4
-local Motor4 = Motor[3]   --负转速
+local Motor1 = Motor[4]   --负转速        3      1
+local Motor2 = Motor[3]   --正转速
+local Motor3 = Motor[2]   --正转速        2      4
+local Motor4 = Motor[1]   --负转速
+
+local pi = 3.1415926
 
 function math.clamp(value,min,max)
     if value<min then
@@ -19,19 +21,19 @@ local trottle = -100
 local M1,M2,M3,M4 = 0,0,0,0
 
 function Motor1_Set()
-    Motor1.setTargetSpeed(-M1)
+    Motor1.setTargetSpeed(math.floor(-denormalize(M1,0,256)))
 end
 
 function Motor2_Set()
-    Motor2.setTargetSpeed(M2)
+    Motor2.setTargetSpeed(math.floor(denormalize(M2,0,256)))
 end
 
 function Motor3_Set()
-    Motor3.setTargetSpeed(M3)
+    Motor3.setTargetSpeed(math.floor(-denormalize(M3,0,256)))
 end
 
 function Motor4_Set()
-    Motor4.setTargetSpeed(-M4)
+    Motor4.setTargetSpeed(math.floor(denormalize(M4,0,256)))
 end
 
 function Motor_Set()
@@ -39,9 +41,86 @@ function Motor_Set()
 end
 
 local Yaw_Angle,Pitch_Angle,Roll_Angle = 0,0,0
-local alt_para = {
-    kp = 8,
-    ki = 0.03,
+local alt_pos = {
+    kp = 0.2,
+    ki = 0,
+    kd = 0,
+    error = 0,
+    err_all = 0,
+    last_err = 0,
+    errall_max = 10000,
+    speed_max = 1000
+}
+
+local alt_spd = {
+    kp = 0.1,
+    ki = 0,
+    kd = 0.01,
+    error = 0,
+    err_all = 0,
+    last_err = 0,
+    errall_max = 10000,
+    speed_max = 256
+}
+
+local yaw_ang = {
+    kp = 0.1,
+    ki = 0,
+    kd = 0,
+    error = 0,
+    err_all = 0,
+    last_err = 0,
+    errall_max = 10000,
+    speed_max = 256
+}
+
+local yaw_rat = {
+    kp = 1.0,
+    ki = 0.00,
+    kd = 0.01,
+    error = 0,
+    err_all = 0,
+    last_err = 0,
+    errall_max = 10000,
+    speed_max = 256
+}
+
+local pos_p = {
+    kp = 0.1,
+    ki = 0.0,
+    kd = 0.0,
+    error = 0,
+    err_all = 0,
+    last_err = 0,
+    errall_max = 10000,
+    speed_max = 10
+}
+
+local spd = {
+    kp = 0.1,
+    ki = 0.001,
+    kd = 0.0,
+    error = 0,
+    err_all = 0,
+    last_err = 0,
+    errall_max = 10000,
+    speed_max = 45
+}
+
+local att_ang = {
+    kp = 0.2,
+    ki = 0.0,
+    kd = 0,
+    error = 0,
+    err_all = 0,
+    last_err = 0,
+    errall_max = 10000,
+    speed_max = 256
+}
+
+local att_rat = {
+    kp = 0.1,
+    ki = 0,
     kd = 0.05,
     error = 0,
     err_all = 0,
@@ -50,42 +129,31 @@ local alt_para = {
     speed_max = 256
 }
 
-local yaw_para = {
-    kp = 0.05,
-    ki = 0,
-    kd = 0,
-    error = 0,
-    err_all = 0,
-    last_err = 0,
-    errall_max = 0,
-    speed_max = 256
-}
+function normalize(value,min,max)
+    if value < min then
+        return 0
+    elseif value > max then
+        return 1
+    else
+        return value/(max-min)
+    end
+end
 
-local pit_para = {
-    kp = 5,
-    ki = 0.01,
-    kd = 0.1,
-    error = 0,
-    err_all = 0,
-    last_err = 0,
-    errall_max = 0,
-    speed_max = 256
-}
+function denormalize(value,min,max)
+    if value < 0  and value > -1 then
+        value = (-max-min)*value
+    elseif value < -1 then
+        value = -max-min
+    elseif value > 1 then
+        value = max-min
+    else 
+        value = (max-min)*value
+    end
+    return value
+end
 
-local rol_para = {
-    kp = 5,
-    ki = 0.01,
-    kd = 0.1,
-    error = 0,
-    err_all = 0,
-    last_err = 0,
-    errall_max = 0,
-    speed_max = 256
-}
-
-function PID_Calc(current,target,para)
+function PID_Calc(target,current,para)
     para.error = target-current
-    --print("err"..para.error)
     para.err_all = math.clamp(para.err_all + para.error,-para.errall_max,para.errall_max)
     local result = para.error * para.kp + para.err_all * para.ki + (para.error - para.last_err) * para.kd
     para.last_err = para.error
@@ -93,21 +161,71 @@ function PID_Calc(current,target,para)
     return result
 end
 
-local alt,yaw,pit,rol
+function math.rad_reverse(rad,reverse)
+    if reverse == true then
+        local result
+        if rad < 0 then
+            result = -rad - pi
+        else
+            result = pi - rad
+        end
+        return result
+    else
+        return rad
+    end
+end
+
+local p_re,r_re = {},{}
+
+function Self_Check()
+    if math.abs(ship.getPitch()) > 2 then
+        p_re = true
+    end
+    if math.abs(ship.getRoll()) > 2 then
+        r_re = true
+    end
+end
+
+local alt,yaw,pit,rol =0,0,0,0
+local pro_n = 4
+
+function TF_P(angle,x,z)
+    return -math.sin(angle) * z - math.cos(angle) * x
+end
+
+function TF_R(angle,x,z)
+    return math.sin(angle) * x - math.cos(angle) * z
+end
+
+
+local Pos_X = -332
+local Pos_Y = 100
+local Pos_Z = 77
 while true do
+    Self_Check()
     local yaw_Angle = ship.getYaw()
-    local pitch_Angle = ship.getPitch()
-    local roll_Angle = ship.getRoll()
+    local pitch_Angle = math.rad_reverse(ship.getPitch(),p_re)
+    local roll_Angle = math.rad_reverse(ship.getRoll(),r_re)
     local pos = ship.getWorldspacePosition()
-    alt = PID_Calc(pos.y,0,alt_para)
-    yaw = PID_Calc(yaw_Angle,0,yaw_para)
-    pit = PID_Calc(pitch_Angle,0,pit_para)
-    rol = PID_Calc(roll_Angle,0,rol_para)
-    print("alt:"..pos.y)
-    M1 = alt + pit - rol
-    M2 = alt - pit + rol
-    M3 = alt - pit - rol
-    M4 = alt + pit + rol
+    local omega = ship.getOmega()
+    local mass = ship.getMass()
+    local vel = ship.getVelocity()
+    local px = PID_Calc(Pos_X,pos.x,pos_p)
+    local pz = PID_Calc(Pos_Z,pos.z,pos_p)
+    local pp = TF_P(yaw_Angle,px,pz)
+    local pr = TF_R(yaw_Angle,px,pz)
+    local vp = TF_P(yaw_Angle,vel.x,vel.z)
+    local vr = TF_R(yaw_Angle,vel.x,vel.z)
+    local loit = normalize(math.sqrt(mass/1.41/pro_n),0,256)    
+    alt = PID_Calc(PID_Calc(Pos_Y,pos.y,alt_pos),vel.y,alt_spd)
+    pit = PID_Calc(PID_Calc(PID_Calc(pp,vp,spd),pitch_Angle,att_ang),omega.x,att_rat)
+    rol = PID_Calc(PID_Calc(PID_Calc(pp,vr,spd),roll_Angle,att_ang),omega.z,att_rat)
+    M1 = alt - pit - rol - yaw + loit   --  - rol
+    M2 = alt + pit + rol - yaw + loit
+    M3 = alt - pit + rol + yaw + loit
+    M4 = alt + pit - rol + yaw + loit
+    --M1,M2,M3,M4 = 0,0,0,0
+    print(pr)
 
     Motor_Set()
 end
